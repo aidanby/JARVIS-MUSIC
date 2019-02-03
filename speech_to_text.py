60d566c1b87b7ff2c1b8c81cf050323894506688
chels <chelsey.kurylo@gmail.com>
2019-02-02 20:32:56 -0500

    translation

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,17 +1,20 @@
 import io
 import os
-import webbroser as wb
-import speech recognition as sr
-import time
-from gtts import gTTS
-from google import search
+#import webbroser as wb
+#import speech_recognition as sr
+#import time
+#from gtts import gTTS
+#from google import search
 
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='QHacks-ce31dc001d82.json'
+
+
 def convert_to_text(file_name):
-    client = speech.SpeechClient('QHacks-ce31dc001d82.json')
+    client = speech.SpeechClient().from_service_account_json('QHacks-ce31dc001d82.json')
 
     with io.open(file_name, 'rb') as audio_file:
         content = audio_file.read()
@@ -19,19 +22,22 @@ def convert_to_text(file_name):
 
     config = types.RecognitionConfig(
         encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
-        sample_rate_hertz=44100,
         language_code='en-US'
     )
 
     response = client.recognize(config, audio)
-	print(response)
-	print(
-    return response.results
-	
-def main():
-	covert_to_text('output-1.flac')
+    translation = ""
+    for result in response.results:
+        translation = translation+result.alternatives[0].transcript+""
+    print(translation)
+    return translation
 
-if __name__ == "__main__";
-	main()
+
+def main():
+    convert_to_text('test2.flac')
+
+
+if __name__ == "__main__":
+    main()
 
 
